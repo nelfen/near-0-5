9303515a74d9340ffb81a8ebf4b6853b9b018896
nelfen <nelfen.dev@gmail.com>
2026-01-16 18:27:50 +0900

    feat: sonner 구현(#22)
    - sonner 구현

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,4 +1,7 @@
 import { useState } from 'react';
+import { Outlet } from 'react-router';
+
+import { Toaster } from '@/components/ui/sonner';
 
 import { Header } from '../layout/Header';
 import { Sidebar } from './Sidebar';
@@ -11,7 +14,11 @@ export default function MainLayout() {
       <Sidebar isOpen={isOpen} onClose={() => setIsOpen(false)} />
       <div className="flex flex-1 flex-col">
         <Header onMenuClick={() => setIsOpen(!isOpen)} />
+        <main className="flex-1">
+          <Outlet />
+        </main>
       </div>
+      <Toaster className="dark" position="top-center" />
     </div>
   );
 }
