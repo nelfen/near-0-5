1fcdced72e45510fb80560828e66291a761be397
quartzs2 <csh001231@gmail.com>
2026-01-31 04:49:04 +0900

    feat: 개발서버가 https로 동작하도록 수정(#58)
    - mkcert 설정

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,12 @@ import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
 import { defineConfig } from 'vite';
+import mkcert from 'vite-plugin-mkcert';
 import svgr from 'vite-plugin-svgr';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), svgr()],
+  plugins: [react(), tailwindcss(), svgr(), mkcert()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
